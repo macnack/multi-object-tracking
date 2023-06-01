663301578292bdeadf957e0aaeae402fd26000a1
Maciej <maciej.krupka@rad4m.com>
2023-06-01 15:15:00 +0200

    fix code

--- hungarian.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from scipy.optimize import linear_sum_assignment
-
-adj_matrix = np.array([[108, 125, 150], [150, 135, 175], [122, 148, 250]])
-row_idx, col_idx = linear_sum_assignment(adj_matrix)
-
-print(col_idx)
-print(row_idx)
-print(adj_matrix[col_idx, row_idx])
