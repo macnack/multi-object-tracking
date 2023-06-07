828216910e2ba56790b2f19b675317ecac300a31
Maciej <maciej.krupka@rad4m.com>
2023-06-07 15:51:03 +0200

    fix project

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2 as cv
 import numpy as np
 from utils_cv import get_iou, get_histogram_correlation
 from scipy.optimize import linear_sum_assignment
+import argparse
 
 
 def load_data_paths(dataset_path):
@@ -14,6 +15,7 @@ def load_data_paths(dataset_path):
 
 
 def get_ssim(source, target):
+    # Structural Similarity
     target_resized = cv.resize(target, dsize=(
         source.shape[1], source.shape[0]))
     return ssim_fun(source, target_resized, data_range=source.max() - source.min(), channel_axis=2)
@@ -26,7 +28,7 @@ def get_center_box(box):
 
 
 def distance_to_probability(bbox1, bbox2, decay_rate=0.1):
-    
+    # Distance probability but not used
     bbox1_center = np.array(get_center_box(bbox1))
     bbox2_center = np.array(get_center_box(bbox2))
     distance = np.linalg.norm(bbox2_center - bbox1_center)
@@ -47,9 +49,9 @@ def combine_probabilities(probabilities, weights=None):
 
 
 def hungarian_algorithm(matrix):
-    row_idx, col_idx = linear_sum_assignment(-matrix)
+    _, col_idx = linear_sum_assignment(-matrix)
     # return ID of most propability object
-    return row_idx, col_idx
+    return col_idx
 
 
 def read_bboxes(filename, prefix):
@@ -72,60 +74,65 @@ def read_bboxes(filename, prefix):
             data[prefix+'/'+str(filename)] = objects
     return data
 
+
 def bipartite_graph(images_path, data, result=None):
-    #iInit
+    # Init
     last_frame = None
-    
-    #To write an output to txt
+
+    # To write an output to txt
     if result is not None:
-        with open(result, 'w') as output:
-            output.close()
+        with open(result, 'w') as output_file:
+            output_file.close()
 
     # creating bibartitle_graph
-    for img_path in images_path:
+    for img_path in images_path[:3]:
         current_objects = data[img_path]
         frame = cv.imread(img_path, cv.IMREAD_COLOR)
-
+        output = []
         if last_frame is not None:
             last_objects = data[last_frame]
             last_frame = cv.imread(last_frame, cv.IMREAD_COLOR)
 
             rows = max(len(last_objects)+1, len(current_objects)+1)
-            #init adjecency matrix
+            # init adjecency matrix
             adjacency_matrix = np.ones(
                 shape=(rows, len(current_objects))) * 0.3
             for id_last, last_bbox in enumerate(last_objects):
+                # Cropping image
                 last_object = last_frame[last_bbox[1]: last_bbox[3],
                                          last_bbox[0]: last_bbox[2], :]
-                row = np.empty((len(current_objects)))
 
                 for id, curr_bbox in enumerate(current_objects):
+                    # Cropping image
                     curr_object = frame[curr_bbox[1]: curr_bbox[3],
                                         curr_bbox[0]: curr_bbox[2], :]
-                    
+
                     iou = np.round(get_iou(last_bbox, curr_bbox), 2)
                     ssim = get_ssim(last_object, curr_object)
                     hist = get_histogram_correlation(last_object, curr_object)
                     adjacency_matrix[id_last, id] = combine_probabilities(
                         [iou, ssim, hist], [0.1, 0.3, 0.6])
 
-            #Optimaze, using hungarian algorithm
-            idx_row, idx_col = hungarian_algorithm(adjacency_matrix.T)
+            # Optimaze, using hungarian algorithm
+            idx_col = hungarian_algorithm(adjacency_matrix.T)
 
-            #Create output buffer
-            output = []
-            for j, i in zip(idx_col, idx_row):
+            # Create output buffer
+
+            for j in idx_col:
                 if j >= len(last_objects):
                     j = -1
                 output.append(j)
-        #Print output buffer
+        if last_frame is None:
+            for _ in data[img_path]:
+                output.append(-1)
+        # Print output buffer
         output_buffer = " ".join(map(str, output)) + '\n'
         print(output_buffer)
 
-        #Make history        
+        # Make history
         last_frame = img_path
 
-        #Easy to compare format with ground_truth.txt
+        # Easy to compare format with ground_truth.txt
         if result is not None:
             output_buffer_file = "\n".join(map(str, output)) + '\n'
             with open(result, 'a') as output_file:
@@ -133,9 +140,17 @@ def bipartite_graph(images_path, data, result=None):
                 output_file.write(f"{len(current_objects)}\n")
                 output_file.write(output_buffer_file)
 
+
 if __name__ == '__main__':
-    path = "frames"
+    parser = argparse.ArgumentParser(
+        description="Process files in a directory and write to an output file")
+    parser.add_argument("directory", nargs="?", default='frames',
+                        help="path to the directory of files")
+    # parser.add_argument("output_file", nargs="?",
+    #                     default='out.csv', help="path to the output file")
+    args = parser.parse_args()
+    path = args.directory
     images_path = load_data_paths(path)
     data = read_bboxes(
         filename="/".join((path, "..", "bboxes.txt")), prefix=path)
-    bipartite_graph(images_path, data, "result.txt")
+    bipartite_graph(images_path, data, result=None)
--- utils_cv.py
+++ utils_cv.py
@@ -1,54 +1,4 @@
-from os.path import isfile, join
-import os
-import numpy as np
 import cv2 as cv
-
-
-images_dir = "dane/data"
-
-def load_images():
-    images = [join(images_dir, f) for f in os.listdir(
-        images_dir) if isfile(join(images_dir, f))]
-    return sorted(images)
-
-def read_img(path, scale):
-    img = cv.imread(path, cv.IMREAD_COLOR)
-    img = cv.resize(img, None, fx=scale, fy=scale)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    return img, gray
-
-def pts_to_corners(pts):
-    left = np.min(pts[:, 0])
-    right = np.max(pts[:, 0])
-    top = np.min(pts[:, 1])
-    bottom = np.max(pts[:, 1])
-    return left, top, right, bottom
-
-
-def show_images_in_grid(images):
-    idx = 0
-    output = np.array([])
-    output_shape = (100, 100)
-    for row in range(int(len(images) / 4)):
-        row_ = np.array([])
-        for col in range(5):
-            if idx < len(images):
-                temp = images[idx]  # cv.imread(images[idx], cv.IMREAD_COLOR)
-                temp = cv.resize(temp, dsize=output_shape)
-                if row_.size == 0:
-                    row_ = temp
-                else:
-                    row_ = np.concatenate((row_, temp), axis=1)
-            else:
-                row_ = np.concatenate(
-                    (row_, np.zeros(temp.shape, dtype=np.uint8)), axis=1)
-            idx += 1
-        if output.size == 0:
-            output = row_
-        else:
-            output = np.concatenate((output, row_), axis=0)
-    return output
-
 
 def show(img, title="One Image"):
     while True:
@@ -58,30 +8,28 @@ def show(img, title="One Image"):
             break
     cv.destroyAllWindows()
 
-def show2(img1, img2, title="Two images"):
-    if img1.ndim != img2.ndim:
-        if img1.ndim > img2.ndim:
-            img2 = cv.cvtColor(img2, cv.COLOR_GRAY2BGR)
-        else:
-            img1 = cv.cvtColor(img1, cv.COLOR_GRAY2BGR)
-    output = np.concatenate((img1, img2), axis=1)
-    show(output, title)
 
 def get_histogram_correlation(source, target):
-    target_resized = cv.resize(target, dsize=(source.shape[1], source.shape[0]))
+    target_resized = cv.resize(target, dsize=(
+        source.shape[1], source.shape[0]))
     source_hist = cv.cvtColor(source, cv.COLOR_BGR2HSV)
     target_hist = cv.cvtColor(target_resized, cv.COLOR_BGR2HSV)
 
     histSize = [50, 60]
     ranges = [0, 180, 0, 256]
     channels = [0, 1]
-    source_hist = cv.calcHist([source_hist], channels=channels, mask=None, histSize=histSize, ranges=ranges, accumulate=False)
-    cv.normalize(source_hist, source_hist, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
+    source_hist = cv.calcHist([source_hist], channels=channels,
+                              mask=None, histSize=histSize, ranges=ranges, accumulate=False)
+    cv.normalize(source_hist, source_hist, alpha=0,
+                 beta=1, norm_type=cv.NORM_MINMAX)
 
-    target_hist = cv.calcHist([target_hist], channels=channels, mask=None, histSize=histSize, ranges=ranges, accumulate=False)
-    cv.normalize(target_hist, target_hist, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
+    target_hist = cv.calcHist([target_hist], channels=channels,
+                              mask=None, histSize=histSize, ranges=ranges, accumulate=False)
+    cv.normalize(target_hist, target_hist, alpha=0,
+                 beta=1, norm_type=cv.NORM_MINMAX)
 
     return cv.compareHist(source_hist, target_hist, cv.HISTCMP_CORREL)
+
 
 def get_iou(boxA, boxB):
     # Calculate coordinates of intersection rectangle
